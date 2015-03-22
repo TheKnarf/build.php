c192e19f595ce578594e42a7043c292e24cbbf45
Frank Lyder bredland <fl@nyggen.com>
2015-03-22 21:46:08 +0100

    Added composer task to the build.php

--- build.php
+++ build.php
@@ -6,8 +6,12 @@
 
 	$build->task('default', ['clean', 'doc'], function() {});
 
-	$build->task('doc', function() {
+	$build->task('doc', ['composer'], function() {
 		$this->exec("./vendor/bin/phpdoc -d ./src -t ./docs");
+	});
+
+	$build->task('composer', function() {
+		$this->exec("composer install");
 	});
 
 	$build->task('clean', function() {
