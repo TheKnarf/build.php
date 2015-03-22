73818887d78e604360cf4777aa0cced7618b8d9a
Frank Lyder bredland <fl@nyggen.com>
2015-03-22 21:20:46 +0100

    Added default task

--- build.php
+++ build.php
@@ -4,12 +4,14 @@
 	
 	$build = new TheKnarf\Buildphp\Build();
 
-	$build->task("doc", function() {
+	$build->task('default', ['clean', 'doc'], function() {});
+
+	$build->task('doc', function() {
 		echo "Building php api documentation\n";
 		exec("./vendor/bin/phpdoc -d ./src -t ./docs");
 	});
 
-	$build->task("clean", function() {
+	$build->task('clean', function() {
 		echo "Cleaning\n";
 		exec("rm -rf docs/");
 	});
